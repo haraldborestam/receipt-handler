6c035a901291a5e69590f7fec1c0a70815b1151d
haraldborestam <harald.borestam@gmail.com>
2024-07-15 10:09:12 +0200

    Add: Spring initializer to backend directory

---
+++ backend/src/main/java/se/harald/receipt/ReceiptHandlerApplication.java
@@ -0,0 +1,13 @@
+package se.harald.receipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptHandlerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/se/harald/receipt/ReceiptHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.harald.receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
